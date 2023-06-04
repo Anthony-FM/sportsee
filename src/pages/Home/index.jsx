dae85f601367845e9d05bd23cacb4b113546cd53
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-06-04 16:25:18 +0200

    :heavy_plus_sign: ajout des composants CardId et du Context ( useContext , isMocked ) :heavy_check_mark:

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,28 @@
+// Composants
+import CardId from '../../components/CardId'
+// CSS
+import './index.css'
+// Context
+import { useContext } from 'react'
+import { MockedContext } from '../../utils/hooks'
+
+
 function Home(){
-    return <div></div>
+
+
+    let { isMocked, toggleContext } = useContext(MockedContext)
+    
+    
+    let buttonText = isMocked === true  ? "Utiliser les données de l'API" : "Utiliser les données Mocké"
+    console.log(isMocked, buttonText)
+
+    return <section className='home-section'>
+        <div className='card-container'>
+            <CardId id="12" gender='Male'/>
+            <CardId id="18" gender='Female'/>
+            <button onClick={() => toggleContext()} className='card-button'>{buttonText}</button>
+        </div>     
+    </section>
 }
 
 export default Home
