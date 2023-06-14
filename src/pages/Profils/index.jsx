8b0532f1c37a84a6541f5e9a12ac8238f51e6fee
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-06-14 10:32:34 +0200

    ✨ Ajouts nouveau composants, 🔥 :zap:  modif et optimisation des données

--- src/pages/Profils/index.jsx
+++ src/pages/Profils/index.jsx
@@ -2,11 +2,11 @@ import { useParams } from "react-router-dom"
 import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { MockedContext } from "../../utils/hooks";
-
+//Hooks
 import { 
     useFetch 
 } from "../../utils/hooks"
-
+//Classes
 import { 
     userData, 
     userActivity, 
@@ -14,9 +14,14 @@ import {
     userPerformance,
     
 } from "../../utils/dataFormatted/dataFormatted";    
-
+//CSS
 import './index.css'
+//Composants
 import Loader from "../../components/Loader";
+import NutritionItem from "../../components/NutritionItem";
+import DailyActivity from "../../components/DailyActivity";
+import RadarChartTypeActivity from "../../components/RadarChartTypeActivity";
+import LineChartAverageSession from "../../components/LineChartAverageSession";
 
 //************************************************** */
 //
@@ -28,66 +33,120 @@ function Profils(){
     let { isMocked } = useContext(MockedContext)
 
     let urlDataOfTheUser = isMocked === false ? `http://localhost:3000/user/${userId}` : `../dataMocked/userMainData${userId}.json`
-   
     let urlDataOfUserActivity = isMocked === false ? `http://localhost:3000/user/${userId}/activity` : `../dataMocked/userActivityData${userId}.json` 
     let urlDataOfUserAverageSession = isMocked === false ? `http://localhost:3000/user/${userId}/average-sessions` : `../dataMocked/userAverageSession${userId}.json` 
     let urlDataOfUserPerformance = isMocked === false ? `http://localhost:3000/user/${userId}/performance` : `../dataMocked/userPerformanceData${userId}.json` 
 
-    console.log(urlDataOfTheUser, '===', isMocked)
+    console.log(urlDataOfTheUser, '=== is data mocked? ===>', isMocked)
     
-    let fetchDataOfTheUser 
-    let fetchDataOfUserActivity 
-    let fetchDataOfUserAverageSession 
-    let fetchDataOfUserPerformance
+    let fetchDataOfTheUser = useFetch(urlDataOfTheUser)
+    let fetchDataOfUserActivity = useFetch(urlDataOfUserActivity)
+    let fetchDataOfUserAverageSession = useFetch(urlDataOfUserAverageSession)
+    let fetchDataOfUserPerformance = useFetch(urlDataOfUserPerformance)
 
-    let dataOfUser
-    let dataOfUserActivity
-    let dataOfUserAverageSession
-    let dataOfUserPerformance
-
-    
-    fetchDataOfTheUser = useFetch(urlDataOfTheUser)
-    fetchDataOfUserActivity = useFetch(urlDataOfUserActivity)
-    fetchDataOfUserAverageSession = useFetch(urlDataOfUserAverageSession)
-    fetchDataOfUserPerformance = useFetch(urlDataOfUserPerformance)
-    
-    // if(fetchDataOfTheUser.error ||
-    //     fetchDataOfUserActivity.error ||
-    //     fetchDataOfUserAverageSession.error ||
-    //     fetchDataOfUserPerformance.error
-    // ){
-    //     return <div>
-    //         Oops! Une errrur est survenu lors du chargement des données
-    //         <Navigate to="*" replace={true} /> 
-    //     </div>
-    // } 
-    
-    
-    !fetchDataOfTheUser.isLoading ? (dataOfUser = new userData(fetchDataOfTheUser.dataUser.data)) : (dataOfUser = null)
-    !fetchDataOfUserActivity.isLoading ? (dataOfUserActivity = new userActivity(fetchDataOfUserActivity.dataUser.data)) : (dataOfUserActivity = null)
-    !fetchDataOfUserAverageSession.isLoading ? (dataOfUserAverageSession = new userSessionAverage(fetchDataOfUserAverageSession.dataUser.data)) : (dataOfUserAverageSession = null)
-    !fetchDataOfUserPerformance.isLoading ? (dataOfUserPerformance = new userPerformance(fetchDataOfUserPerformance.dataUser.data)) : (dataOfUserPerformance = null)
+    if(fetchDataOfTheUser.dataUser === "can not get user" || 
+        fetchDataOfUserActivity.error ||
+        fetchDataOfUserAverageSession.error ||
+        fetchDataOfUserPerformance.error
+    ){
+        return <div>
+            <Navigate to="*" replace={true} />            
+        </div>
+    } 
     console.log(fetchDataOfTheUser)
 
-    console.log(dataOfUser)
-    console.log(dataOfUserActivity)
-    console.log(dataOfUserAverageSession)
-    console.log(dataOfUserPerformance)
+    let dataOfUser = fetchDataOfTheUser.isLoading === false ? new userData(fetchDataOfTheUser.dataUser.data) :  null;
+    let dataOfUserActivity = fetchDataOfUserActivity.isLoading === false ?  new userActivity(fetchDataOfUserActivity.dataUser.data) : null;
+    let dataOfUserAverageSession =  fetchDataOfUserAverageSession.isLoading === false ?  new userSessionAverage(fetchDataOfUserAverageSession.dataUser.data) : null
+    let dataOfUserPerformance =  fetchDataOfUserPerformance.isLoading === false ? new userPerformance(fetchDataOfUserPerformance.dataUser.data) : null
+    
+    
 
+    
+    return (
+        fetchDataOfTheUser.error ||
+        fetchDataOfUserActivity.error ||
+        fetchDataOfUserAverageSession.error ||
+        fetchDataOfUserPerformance.error
+        ) ?
 
-    
-    
-    return fetchDataOfTheUser.isLoading === undefined ?
-    (<Navigate to="*" replace={true} />) :
-    
-    fetchDataOfTheUser.isLoading ? 
-    <Loader/> : 
-    
-    <section>
-       
-        <h1 className="firstName-title">Bonjour <span className="firstName-name">{dataOfUser.firstName}</span></h1>
-        <p className="firstName-text">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+        (<Navigate to="*" replace={true} />) :
+        
+        (
+            dataOfUser === null ||
+            dataOfUserActivity === null ||
+            dataOfUserAverageSession === null ||
+            dataOfUserPerformance === null 
+        ) ? 
+        <Loader/> : 
+        
+        <section className="profil-section">
+            <div className="profil-title">
+                <h1 className="firstName-title">Bonjour <span className="firstName-name">{dataOfUser.firstName}</span></h1>
+                <p className="firstName-text">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+            </div>
+            <div className="profil-container">
+                <div className="dailyActivity-container ">
+                    <div className="titleDailyActivity">
+                        <h2>Activité quotidienne</h2>
+                        <div className="lengends">
+                            <div className="legends-container">
+                                <div className="dot black"></div>
+                                <p className="legends-text">Poids (kg)</p>
+                            </div>
+                            <div className="legends-container">
+                                <div className="dot red"></div>
+                                <p className="legends-text">Calories brûlées (kCal)</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                        <DailyActivity sessions={dataOfUserActivity.sessions} />
+                    
+                
+                </div>
+                <div className="trainingSession-container">
+                    <div className="lineChart-container">
+                        
+                        <p className="legendText">Durée moyenn des sessions</p>                    
+                        
+                        <LineChartAverageSession data={dataOfUserAverageSession}/>
+                        
+                    </div>
+                    <div className="radarChart-container">
+                        
+                        <RadarChartTypeActivity data={dataOfUserPerformance}/>
+                        
+                    </div>
+                    <div className="radialBarChart-container">
 
-    </section>
+                    </div>
+                </div>
+                
+                <div className='nutrition-container'>
+                    <NutritionItem 
+                        value={dataOfUser.calories}
+                        name='Calories'
+                    />
+                    <NutritionItem 
+                        value={dataOfUser.proteines}
+                        name='Proteines'
+                    />
+                    <NutritionItem 
+                        value={dataOfUser.glucides}
+                        name='Glucides'
+                    />
+                    <NutritionItem 
+                        value={dataOfUser.lipides}
+                        name='Lipides'
+                    />
+
+                </div>
+                
+            </div>
+            
+            
+
+        </section>
 }
 export default Profils
