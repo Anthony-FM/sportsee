ffd980579aef4bbf5068c1810ad97aaba49e7bc6
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-06-19 09:42:55 +0200

    🎨 Modif margin et couleur radar

--- src/components/RadarChartTypeActivity/index.jsx
+++ src/components/RadarChartTypeActivity/index.jsx
@@ -5,11 +5,11 @@ import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, Responsi
 function RadarChartTypeActivity({data}){
     console.log(data.dataPerformance)
     return <ResponsiveContainer width="100%" height="100%" >
-        <RadarChart   data={data.dataPerformance} margin={{top: 5, right: 50, left: 50, bottom: 5 }}>
+        <RadarChart   data={data.dataPerformance} margin={{top: 5, right: 60, left: 60, bottom: 5 }}>
             <PolarGrid gridType="polygon" radialLines={false} />
             <PolarAngleAxis dataKey="kind" tick={{ fill: "white", fontSize: 12 }} />
             <PolarRadiusAxis angle={30} domain={[0, 150]} tick={false} axisLine={false} />
-            <Radar dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            <Radar dataKey="value"  fill="#FF0000" fillOpacity={0.6} />
         </RadarChart>
     </ResponsiveContainer>
 }
