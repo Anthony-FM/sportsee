f20392c3d6bb37fffd13332a972bab050e9fb603
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-06-19 09:44:26 +0200

    :fire: :zap: Modif et amélioration compréhension des données

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,14 +13,18 @@ function Home(){
     let { isMocked, toggleContext } = useContext(MockedContext)
     
     
-    let buttonText = isMocked === true  ? "Utiliser les données de l'API" : "Utiliser les données Mocké"
+    let buttonText = isMocked === true  ? "Activer les données API" : "Activer les données Mockée"
+    let text = isMocked ? "Vous êtes sur les données mockée:" : "Vous êtes sur les données API:"
     console.log(isMocked, buttonText)
 
     return <section className='home-section'>
         <div className='card-container'>
             <CardId id="12" gender='Male'/>
             <CardId id="18" gender='Female'/>
-            <button onClick={() => toggleContext()} className='card-button'>{buttonText}</button>
+            <div className='toggleContainer'>
+                <p>{text}</p>
+                <button onClick={() => toggleContext()} className='card-button'>{buttonText}</button>
+            </div>
         </div>     
     </section>
 }
